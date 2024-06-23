ed2783c97df80b3739047e3739a0d1dd1846bc94
thangnq <thangng1@vnpay.vn>
2024-06-23 10:39:07 +0700

    add main layout

---
+++ src/conponents/MainLayout/index.tsx
@@ -0,0 +1,117 @@
+import React, { useState } from 'react';
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
+import {Link} from "react-router-dom";
+import { HomeOutlined, IdcardOutlined, LineChartOutlined, TeamOutlined, UserOutlined} from "@ant-design/icons";
+
+const { Header, Content, Footer, Sider } = Layout;
+
+type MenuItem = {
+    key: any;
+    label: React.ReactNode;
+    link?: string;
+    icon?: React.ReactNode;
+    children?: MenuItem[];
+};
+
+const buildMenuItem = ({
+   key,
+   label,
+   link,
+   icon,
+   children,
+}: {
+    key: string;
+    label: React.ReactNode;
+    link: string;
+    icon?: React.ReactNode;
+    children?: MenuItem[];
+}): MenuItem => {
+    const item = {
+        key,
+        label,
+        link,
+        icon,
+        children,
+    } as MenuItem;
+    const hasChildren = children && children.length > 0;
+
+    // Leaf item
+    if (link && !hasChildren) {
+        item.label = <Link to={link}>{label}</Link>;
+    }
+
+    return item;
+};
+
+const allItems: MenuItem[] = [
+    buildMenuItem({
+        key: "1",
+        label: "Phòng",
+        link: "",
+        icon: <HomeOutlined />
+    }),
+    buildMenuItem({
+        key: "2",
+        label: "Khách hàng",
+        link: "",
+        icon: <TeamOutlined />
+    }),
+    buildMenuItem({
+        key: "3",
+        label: "Nhân viên",
+        link: "",
+        icon: <UserOutlined />
+    }),
+    buildMenuItem({
+        key: "4",
+        label: "Báo cáo",
+        link: "",
+        icon: <LineChartOutlined />
+    }),
+    buildMenuItem({
+        key: "5",
+        label: "Profile",
+        link: "",
+        icon: <IdcardOutlined />
+    }),
+];
+
+const MainLayout: React.FC = () => {
+    const [collapsed, setCollapsed] = useState(false);
+    const {
+        token: { colorBgContainer, borderRadiusLG },
+    } = theme.useToken();
+
+    return (
+        <Layout style={{ minHeight: '100vh' }}>
+            <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+                {/*logo is here*/}
+                <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={allItems} />
+            </Sider>
+            <Layout>
+                <Header style={{ padding: 0, background: colorBgContainer }} />
+                <Content style={{ margin: '0 16px' }}>
+                    <Breadcrumb style={{ margin: '16px 0' }}>
+                        <Breadcrumb.Item>User</Breadcrumb.Item>
+                        <Breadcrumb.Item>Bill</Breadcrumb.Item>
+                    </Breadcrumb>
+                    <div
+                        style={{
+                            padding: 24,
+                            minHeight: 360,
+                            background: colorBgContainer,
+                            borderRadius: borderRadiusLG,
+                        }}
+                    >
+                        Bill is a cat.
+                    </div>
+                </Content>
+                <Footer style={{ textAlign: 'center' }}>
+                    Ant Design ©{new Date().getFullYear()} Created by Ant UED
+                </Footer>
+            </Layout>
+        </Layout>
+    );
+};
+
+export default MainLayout;
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,17 @@
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import {router} from "./conponents/routes";
+import {RouterProvider} from "react-router-dom";
+import { ConfigProvider } from 'antd';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <div>Hello world</div>
+    <ConfigProvider
+        theme={{
+            token: {
+                fontFamily: "Be Vietnam Pro",
+            },
+        }}
+    >
+        <RouterProvider router={router} />
+    </ConfigProvider>
 )
