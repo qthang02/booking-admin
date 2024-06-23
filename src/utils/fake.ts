7467e27e778baf979c09d9f4394f6dd4f73d9646
thangnq <thangng1@vnpay.vn>
2024-06-23 16:51:38 +0700

    edit ts config

---
+++ src/utils/fake.ts
@@ -0,0 +1,5 @@
+export function GetRandomIntInclusive(min: number, max: number): number {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
+}
